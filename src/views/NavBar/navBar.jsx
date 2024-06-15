a27c9b464c18a808b56e0599d903cbc1b8f233db
BRIWA <salaheddinezouitni00@gmail.com>
2024-06-15 13:35:17 +0100

    feature : responsivnesse bugs fixed, form control added

--- src/views/NavBar/navBar.jsx
+++ src/views/NavBar/navBar.jsx
@@ -1,8 +1,6 @@
 import { Link } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
-
 import { useState } from 'react';
-import { FiMenu } from "react-icons/fi";
 
 const ScrollLink = ({ to, smooth, duration, offset, children }) => {
     return (
@@ -28,12 +26,10 @@ const Links = () => {
          ];
 
     return (
-        <div className="flex flex-col md:flex-row gap-8 items-center">
+        <div className="md:flex hidden flex-row gap-8 items-center">
             {links.map((link)=> 
                 <ScrollLink to={link.route} smooth={true} duration={700} offset={-30} >{link.name}</ScrollLink>
             )}
-            <RouterLink to="" className="border p-2 rounded-md px-4 border-[#3C5065]">Contact</RouterLink>
-            <RouterLink to="" className="border p-2 rounded-md px-4 border-[#3C5065] text-white bg-[#3C5065]">Download Resume</RouterLink>
         </div>
     );
 }
@@ -42,14 +38,13 @@ const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="fixed top-0 left-0 right-0 bg-[#E7EFF7] z-10 shadow-md flex justify-between md:items-center p-3">
+        <nav className="fixed top-0 left-0 right-0 bg-[#E7EFF7] z-10 shadow-md flex justify-between p-3">
             <img src="logo.png" alt="logo" className="h-8" />
-            <div className={`md:flex ${isOpen ? 'block' : 'hidden'}`}>
+            <div className={`flex md:gap-6 gap-2`}>
                 <Links></Links>
+                    <Link as={ScrollLink} to="contact" smooth={true} duration={700} offset={-30} className="border p-2 rounded-md px-4 border-[#3C5065] cursor-pointer">Contact</Link>
+                    <RouterLink to="" className="border p-2 rounded-md px-4 border-[#3C5065] text-white bg-[#3C5065]">Download Resume</RouterLink>
             </div>
-            <button onClick={() => setIsOpen(!isOpen)} className="md:hidden h-8">
-                <FiMenu className='h-10'/>
-            </button>
         </nav>
     );
 }
