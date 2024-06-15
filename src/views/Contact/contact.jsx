576ebae0e6b20a4ef55a5b65049cd719228f92bd
BRIWA <salaheddinezouitni00@gmail.com>
2024-06-15 12:01:39 +0100

    feature : contact (logic added)

--- src/views/Contact/contact.jsx
+++ src/views/Contact/contact.jsx
@@ -3,12 +3,17 @@ import { SectionHeading } from '../../components'
 import { IoMdMail } from "react-icons/io";
 import { MdLocalPhone } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
-
-
-
-
 const contact = () => {
     const [acceptTerms, setAcceptTerms] = useState(false);
+    const [message, setMessage] = useState('');
+    const [isTouched, setIsTouched] = useState(false);
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+        if (!isTouched) {
+          setIsTouched(true);
+        }
+    };
   return (
     <div className="flex flex-col p-8 gap-7" id="about">
         <SectionHeading
@@ -34,7 +39,7 @@ const contact = () => {
                     <p>Paris, France</p>
                 </div>
             </div>
-            <div className='flex flex-col items-start gap-5'>
+            <form action="https://public.herotofu.com/v1/d19abc40-2a94-11ef-baa4-2f3d60870fc4" method="post" accept-charset="UTF-8" className='flex flex-col items-start gap-5'>
                 <div className='flex flex-col gap-3 w-full'>
                     <label htmlFor="name"> Name</label>
                     <input type="text" name="name" id="name" className='border border-[#3C5065] p-2 rounded-lg' />
@@ -43,14 +48,16 @@ const contact = () => {
                     <input type="email" name="email" id="email" className='border border-[#3C5065] p-2 rounded-lg' />
 
                     <label htmlFor="message"> Message</label>
-                    <textarea name="message" rows={5} placeholder='Type your message' id="message" className='border border-[#3C5065] p-2 rounded-lg' />
+                    <textarea name="message" rows={5} placeholder='Type your message' id="message" onChange={handleChange} className='border border-[#3C5065] p-2 rounded-lg' />
+                    {isTouched && !message && <p className="text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> message is required!</p>}
+
                 </div>
                 <div className='flex items-center mt-4'>
-                    <input type="checkbox" id="terms" className='mr-2' onChange={(e) => setAcceptTerms(e.target.checked)} />
+                    <input type="checkbox" id="terms" name='terms' className='mr-2' onChange={(e) => setAcceptTerms(e.target.checked)} />
                     <label htmlFor="terms">I accept the terms</label>
                 </div>
-                <button className={`bg-[#3C5065] text-white rounded-xl p-2 px-6 ${!acceptTerms && 'opacity-70'}`} disabled={!acceptTerms}>Submit</button>
-            </div>
+                <button className={`bg-[#3C5065] text-white rounded-xl p-2 px-6 ${!acceptTerms || !message ? 'opacity-70' : ''}`} disabled={!acceptTerms || !message}>Submit</button>
+            </form>
         </div>
     </div>
   )
