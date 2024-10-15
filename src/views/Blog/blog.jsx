33529481649d54ad506318f13697e17d836fc5ba
zouhir <rajdaouizouhir.pro@gmail.com>
2024-10-15 21:19:23 +0200

    feat: Add blogs horizontal scroll.

--- src/views/Blog/blog.jsx
+++ src/views/Blog/blog.jsx
@@ -1,46 +1,40 @@
+import React, { useRef } from "react";
 import { SectionHeading } from "../../components";
 
 const Blog = ({ image, category, title, description, date }) => {
   return (
-    <div className="flex flex-col items-center rounded-lg border border-[#3C5065]">
-      <img src="blog.png" alt={title} className="w-full aspect-[5/3]" />
-      <div className="flex flex-col p-2">
-        <p className="text-base">{category}</p>
-        <p className="font-bold text-xl mt-2">{title}</p>
-        <p className="text-sm font-light mt-2">{description}</p>
-        <p className="text-sm font-light mt-6">{date}</p>
+    <div className="flex-none w-full sm:w-1/2 md:w-1/3 rounded-lg border border-[#3C5065]">
+      <div className="flex flex-col items-center">
+        <img src="blog.png" alt={title} className="w-full aspect-[5/3]" />
+        <div className="flex flex-col p-2">
+          <p className="text-base">{category}</p>
+          <p className="font-bold text-xl mt-2">{title}</p>
+          <p className="text-sm font-light mt-2">{description}</p>
+          <p className="text-sm font-light mt-6">{date}</p>
+        </div>
       </div>
     </div>
   );
 };
 
 const Blogs = () => {
-  const blogs = [
-    {
-      image: "blog.png",
-      category: "category",
-      title: "Blog title heading will go here",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-      date: "11 Jan 2022. 5 min read",
-    },
-    {
-      image: "blog.png",
-      category: "category",
-      title: "Blog title heading will go here",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-      date: "11 Jan 2022. 5 min read",
-    },
-    {
-      image: "blog.png",
-      category: "category",
-      title: "Blog title heading will go here",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-      date: "11 Jan 2022. 5 min read",
-    },
-  ];
+  const scrollContainerRef = useRef(null);
+
+  // Scroll left function
+  const scrollLeft = () => {
+    scrollContainerRef.current.scrollBy({
+      left: -300, // Adjust scroll distance here
+      behavior: "smooth",
+    });
+  };
+
+  // Scroll right function
+  const scrollRight = () => {
+    scrollContainerRef.current.scrollBy({
+      left: 300, // Adjust scroll distance here
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div className="flex flex-col flex-wrap space-y-10 p-10" id="blog">
@@ -49,19 +43,107 @@ const Blogs = () => {
         subtitle="I write some helpful articles"
         description=""
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-        {blogs.map((blog) => (
-          <Blog
-            category={blog.category}
-            image={blog.icon}
-            title={blog.title}
-            description={blog.description}
-            date={blog.date}
-          />
-        ))}
+      <div ref={scrollContainerRef} className="w-full overflow-x-auto">
+        <div className="flex space-x-4">
+          {blogs.map((blog) => (
+            <Blog
+              category={blog.category}
+              image={blog.icon}
+              title={blog.title}
+              description={blog.description}
+              date={blog.date}
+            />
+          ))}
+        </div>
+      </div>
+      {/* Scroll buttons */}
+      <div className="flex justify-center space-x-2 mt-4">
+        <button
+          onClick={scrollLeft}
+          className="text-white bg-[#3C5065] hover:bg-[#65717E] font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={scrollRight}
+          className="text-white bg-[#3C5065] hover:bg-[#65717E] font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
 };
+
+const blogs = [
+  {
+    image: "blog.png",
+    category: "category",
+    title: "Blog title heading will go here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    date: "11 Jan 2022. 5 min read",
+  },
+  {
+    image: "blog.png",
+    category: "category",
+    title: "Blog title heading will go here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    date: "11 Jan 2022. 5 min read",
+  },
+  {
+    image: "blog.png",
+    category: "category",
+    title: "Blog title heading will go here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    date: "11 Jan 2022. 5 min read",
+  },
+  ,
+  {
+    image: "blog.png",
+    category: "category",
+    title: "Blog title heading will go here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    date: "11 Jan 2022. 5 min read",
+  },
+  ,
+  {
+    image: "blog.png",
+    category: "category",
+    title: "Blog title heading will go here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+    date: "11 Jan 2022. 5 min read",
+  },
+];
 
 export default Blogs;
